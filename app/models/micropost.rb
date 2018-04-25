bf91cfb499979465fff163d9b800f9d1c528e12a
avishag <avishag.csillag@fiverr.com>
2018-04-25 11:22:10 +0300

    Finish toy app2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
 	belongs_to :user
-	validates :content, length: { minimum: 10, maximum: 140 }
+	validates :content, length: { minimum: 10, maximum: 140 },
 				presence: true
 end
